599b1536ae7c3ab4a47294f21b2658bb784b109d
Tracey Jaquith <tracey@archive.org>
2025-02-17 15:06:25 -0800

    upgraded off jquery, and hugo/blogtini compatible

--- js/slide-responsively.js
+++ js/slide-responsively.js
@@ -1,48 +1,43 @@
-/**************************************************************
-
+/* *************************************************************
   slide-responsively:
   Inspired by wordpress "sliding-door" theme.
-  Rewritten entirely to be responsive! 
-  (and using minimal jQuery, not mootools)
-  by @tracey_pooh May 2015
+  Rewritten entirely to be responsive!
+  (using (mostly) CSS + some vanilla JS, not mootools)
+  by @tracey_pooh 2015 and 2025
   https://github.com/traceypooh/slide-responsively
+************************************************************* */
 
-**************************************************************/
+let retries_slide = 1
 
-( function ($) {
-  $(document).ready(function(){
-    // DOM ready, page still (likely) loading images, etc...
-    $('.slide-responsively li').mouseenter(function(e){
+const main = () => {
+  // If using blogtini.com, we have to find this through a shadow dom.
+  // NOTE: wheh blogtin & shadowDOM is involved, 1st or 2nd run of this might be empty list OR
+  // elements that still are "settling" and 1st round of `addEventListeners()` get reset as the
+  // elements repaint/re-render.  So we'll run this a few times to ensure listeners get attached.
+  const els = document.querySelector('bt-post-full')?.shadowRoot?.querySelectorAll('.slide-responsively li') ||
+    document.querySelectorAll('.slide-responsively li')
+
+  console.log({ retries_slide, els })
+
+  // eslint-disable-next-line no-plusplus
+  if (retries_slide++ <= 3)
+    setTimeout(main, 1000 * retries_slide)
+
+  els.forEach((li) => {
+    // eslint-disable-next-line prefer-arrow-callback
+    li.addEventListener('mouseenter', function slide_hover() {
       // user has hovered over a nav img.  (class) mark it "in" and the others "out"
-      $('.slide-responsively li').addClass('out');
-      $(this).removeClass('out').addClass('in');
-    });
-    $('.slide-responsively li').mouseleave(function(e){
+      els.forEach((e) => e.classList.add('out'))
+      li.classList.remove('out')
+      li.classList.add('in')
+    })
+    // eslint-disable-next-line prefer-arrow-callback
+    li.addEventListener('mouseleave', function slide_leave() {
       // user has stopped hovering.  remove all "out", then "in", class markers.
-      $('.slide-responsively li').removeClass('out');    
-      $(this).removeClass('in');
-    });
-  });
-}) ( jQuery );
+      els.forEach((e) => e.classList.remove('out'))
+      li.classList.remove('in')
+    })
+  })
+}
 
-/*
-     FILE ARCHIVED ON 17:07:02 Mar 06, 2019 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 04:29:07 Sep 21, 2019.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  PetaboxLoader3.datanode: 1571.878 (5)
-  exclusion.robots: 0.417
-  RedisCDXSource: 6.87
-  LoadShardBlock: 158.886 (3)
-  CDXLines.iter: 17.025 (3)
-  captures_list: 187.507
-  load_resource: 1536.913
-  PetaboxLoader3.resolve: 92.365 (2)
-  exclusion.robots.policy: 0.397
-  esindex: 0.018
-*/
+document.addEventListener('DOMContentLoaded', main)
